edb1427742ed6d7269b10139050c6fd33af3c839
KarinaTiurina <rktiurina@gmail.com>
2017-08-31 23:15:57 +0300

    author subscription error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user_can_edit?
+  helper_method :own_event?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
     model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
     )
+  end
+
+  def own_event?(event)
+    event.user == current_user
   end
 end
