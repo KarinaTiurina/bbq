bef045c8e0599d45acde50f641e3eb0616d5adec
KarinaTiurina <rktiurina@gmail.com>
2017-09-01 12:22:05 +0300

    i18n subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,7 +7,7 @@ class Subscription < ApplicationRecord
   validates :user_name, presence: true, unless: 'user.present?'
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: 'user.present?'
   validates :user_email, exclusion: { in: :emails,
-                                      message: "%{value} уже существует" }, unless: 'user.present?'
+                                      message: I18n.t('subscriptions.subscription.email_error') }, unless: 'user.present?'
 
   validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
 
