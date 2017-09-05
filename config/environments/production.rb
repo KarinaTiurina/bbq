ba7c331d4b89e317f9c01d504a150bb489aa08b0
KarinaTiurina <rktiurina@gmail.com>
2017-09-05 08:31:11 +0300

    set bucket region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,15 +89,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-
-
   config.action_mailer.default_url_options = {host: 'best-events.herokuapp.com'}
-
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_deliveries = true
-
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
